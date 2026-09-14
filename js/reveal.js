20f83dca4fe60dc605846587bc8f97f79c776724
Miguelle Febres <guellefeb21@gmail.com>
2026-09-14 13:48:18 -0500

    Update

--- js/reveal.js
+++ js/reveal.js
@@ -3,7 +3,7 @@
    Triggering per-section (not per-element) means grid/carousel items that sit
    off-screen horizontally still reveal once you reach the section. */
 (function () {
-  var SEL = '.topper,.hero__wordmark,.hero__text,.hero__actions,' +
+  var SEL = '.topper,.hero__title,.hero__text,.hero__actions,' +
             '.programs__title,.programs__text,.program-card,' +
             '.gallery__title,.gallery__text,.gallery__item,' +
             '.services__title,.services__text,.service-card,' +
